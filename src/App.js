eb312d586437aa8af2c7ab8033ad5166b7370e37
MorganRohan <10355@coderacademy.edu.au>
2022-11-12 17:20:35 +1000

    added state content and updated log in component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import Register from "./components/Register";
 import LogIn from "./components/LogIn";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Routes, Route } from "react-router-dom";
 import AddRoster from "./components/AddRoster";
 import Dashboard from "./components/Dashboard";
 
 function App() {
-
   return (
     <main className="App">
-      <LogIn />
-      <Route exact path="/dashboard" element={Dashboard}/>
-      <Route exact path="/addroster" element={AddRoster}/>
+      <Routes>
+        <Route exact path="/login" element={<LogIn />}/>
+        <Route exact path="/register" element={<Register />}/>
+        <Route exact path="/dashboard" element={<Dashboard />}/>
+        <Route exact path="/addroster" element={<AddRoster />}/>
+      </Routes>
     </main>
   );
 }
--- src/components/LogIn.js
+++ src/components/LogIn.js
@@ -1,108 +1,60 @@
-import { useRef, useState, useEffect, useContext } from 'react';
-import AuthContext from "../contexts/AuthContext";
-import { Container } from "@mui/material";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { AuthProvider } from "../contexts/AuthContext";
+import { useGlobalState } from "../contexts/StateContent";
+import { Button } from "@mui/material";
 
-import axios from '../config/api.js';
-const LOGIN_URL = '/auth';
+export default function Login() {
+  const initialFormState = {
+    email: "",
+    password: "",
+  };
 
-const LogIn = () => {
-    const { setAuth } = useContext(AuthContext);
-    const userRef = useRef();
-    const errRef = useRef();
+  const [formState, setFormState] = useState(initialFormState);
 
-    const [user, setUser] = useState('');
-    const [pwd, setPwd] = useState('');
-    const [errMsg, setErrMsg] = useState('');
-    const [success, setSuccess] = useState(false);
+  const { dispatch } = useGlobalState();
+  let navigate = useNavigate();
 
-    useEffect(() => {
-        userRef.current.focus();
-    }, [])
+  function handleChange(event) {
+    setFormState({
+      ...formState,
+      [event.target.name]: event.target.value,
+    });
+  }
 
-    useEffect(() => {
-        setErrMsg('');
-    }, [user, pwd])
+  function handleSubmit(event) {
+    event.preventDefault();
 
-    const handleSubmit = async (e) => {
-        e.preventDefault();
+    AuthProvider(formState)
+      .then((data) => {
+        let username = data.username;
+        let token = data.token;
+        sessionStorage.setItem("token", token);
+        sessionStorage.setItem("user", username);
 
-        try {
-            const response = await axios.post(LOGIN_URL,
-                JSON.stringify({ user, pwd }),
-                {
-                    headers: { 'Content-Type': 'application/json' },
-                    withCredentials: true
-                }
-            );
-            console.log(JSON.stringify(response?.data));
-            //console.log(JSON.stringify(response));
-            const accessToken = response?.data?.accessToken;
-            const roles = response?.data?.roles;
-            setAuth({ user, pwd, roles, accessToken });
-            setUser('');
-            setPwd('');
-            setSuccess(true);
-        } catch (err) {
-            if (!err?.response) {
-                setErrMsg('No Server Response');
-            } else if (err.response?.status === 400) {
-                setErrMsg('Missing Username or Password');
-            } else if (err.response?.status === 401) {
-                setErrMsg('Unauthorized');
-            } else {
-                setErrMsg('Login Failed');
-            }
-            errRef.current.focus();
-        }
-    }
-
-    return (
-        <Container>
-            {success ? (
-                <section>
-                    <h1>You are logged in!</h1>
-                    <br />
-                    <p>
-                        <a href="#">Go to Home</a>
-                    </p>
-                </section>
-            ) : (
-                <section>
-                    <p ref={errRef} className={errMsg ? "errmsg" : "offscreen"} aria-live="assertive">{errMsg}</p>
-                    <h1>Sign In</h1>
-                    <form onSubmit={handleSubmit}>
-                        <label htmlFor="username">Username:</label>
-                        <input
-                            type="text"
-                            id="username"
-                            ref={userRef}
-                            autoComplete="off"
-                            onChange={(e) => setUser(e.target.value)}
-                            value={user}
-                            required
-                        />
-
-                        <label htmlFor="password">Password:</label>
-                        <input
-                            type="password"
-                            id="password"
-                            onChange={(e) => setPwd(e.target.value)}
-                            value={pwd}
-                            required
-                        />
-                        <button>Sign In</button>
-                    </form>
-                    <p>
-                        Need an Account?<br />
-                        <span className="line">
-                            {/*put router link here*/}
-                            <a href="#">Sign Up</a>
-                        </span>
-                    </p>
-                </section>
-            )}
-        </Container>
-    )
+        dispatch({ type: "setLoggedInUser", data: username });
+        dispatch({ type: "setToken", data: token });
+        navigate("/");
+      })
+      .catch((error) => console.log(error));
+  }
+  return (
+    <div>
+      <label>Email:</label>
+      <input
+        type="email"
+        name="email"
+        value={formState.username}
+        onChange={handleChange}
+      ></input>
+      <label>Password:</label>
+      <input
+        type="password"
+        name="password"
+        value={formState.password}
+        onChange={handleChange}
+      ></input>
+      <Button onClick={handleSubmit}>Login</Button>
+    </div>
+  );
 }
-
-export default LogIn
