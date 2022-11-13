7611e6a9968b45548ded8e1825798f3faa60218c
MorganRohan <10355@coderacademy.edu.au>
2022-11-13 16:08:59 +1000

    question numbers and next button handled

--- src/components/PayCalc.js
+++ src/components/PayCalc.js
@@ -2,18 +2,44 @@ import React from "react";
 import PayEstimate from "./PayEstimate";
 import PayQuestions from "./PayQuestions";
 
-function PayCalc() {
+export const questions = [
+    "Job Position",
+    "Rostered Hours"
+]
 
+class PayCalc extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            number: 0
+        };
+
+        // Binding so that `this` can be used
+        this.incrementNumber = this.incrementNumber.bind(this);
+    }
+
+    incrementNumber() {
+        this.setState((state, props) => ({
+            number: state.number + 1
+        }))
+    }
+
+    render() {
     const isComplete = false;
     return(
         <div id="paycalc">
             <h1>Pay Calculator</h1>
             <h2>Complete the calculator below for a pay estimate</h2>
 
-        { isComplete ? <PayEstimate /> : <PayQuestions /> }
+        {isComplete ? (
+            <PayEstimate /> 
+        ) : (
+            <PayQuestions questionNumber={this.state.number} incrementNumber={this.incrementNumber}/> 
+        )}
         </div>
-
-    )
-}
+        );
+    };
+};
 
 export default PayCalc
