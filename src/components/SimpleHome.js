d632a934cd00b1b51af5334b94d4bf63d6c7ffbe
MorganRohan <10355@coderacademy.edu.au>
2022-11-18 08:40:36 +1000

    changes made to allow for authenticated CRUD functionality for rosters

--- src/components/SimpleHome.js
+++ src/components/SimpleHome.js
@@ -1,12 +1,14 @@
 import React from "react";
 import LogIn from "./LogIn";
+import Rosters from "./Rosters";
 
 const Home = () => {
     return (
         <section id="home">
             <div>
                 <h1>Log in to enter the portal....</h1>
-                {/* <LogIn /> */}
+                <LogIn />
+                <Rosters />
                 {/* <img src="../images/entertheportal.jpg" alt="door opening" height={600} width={1100}></img> */}
             </div>
         </section>
